05bb6b702f8df0560cb335491946277226e34dd6
Bhargavkumar Patel <bhargav079@gmail.com>
2017-04-07 15:34:09 +0530

    Added authentication models.

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from authentication.models import Profile
 
 # Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ('user','mobile','address')
+	# list_filter = ('mobile',)
+	search_fields = ['user__username','user__first_name','user__last_name','mobile','address']
+
+admin.site.register(Profile,ProfileAdmin)
--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User)
+	mobile = models.CharField(max_length=15)
+	address = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.user.username+" | "+self.user.email
